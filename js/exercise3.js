7fb3f9564943ca53ac63bd303d7044141b27d176
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-10-19 19:50:29 -0500

    3 and 4 exercises

---
+++ js/exercise3.js
@@ -0,0 +1,8 @@
+const numbers = [3, 12, 7, 19];
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  total += numbers[i];
+}
+
+console.log(total);
--- js/exercise4.js
+++ js/exercise4.js
@@ -1,4 +1,4 @@
-const list = [12, 6, 9, 34, 68];
-for (let i = 0; i < list.length; i++) {
-  console.log(i);
+let list = [1, 2, 3, 4, 5];
+for (let i = 0; i <= list.length - 1; i++) {
+  console.log(list[i]);
 }
